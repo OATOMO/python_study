c172c66c3cc00587030a44e60f8b6cfec47dd3b3
OATOMO <443437061@qq.com>
2017-05-02 19:54:07 +0800

    get jpg

--- python/project_py/jpg/get_jpg.py
+++ python/project_py/jpg/get_jpg.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 #------------------------------------------
+#
+#有bug，会抓到错误的url`
+#
 import re
 from urllib import request
 import urllib
+from tqdm import tqdm 
 
 url_dota = 'https://tieba.baidu.com/p/5097345368'
 
@@ -18,11 +22,13 @@ def getJpg(html):
     """
     获取图像
     """
-    res = r'https://.+\.jpg'
+    res = """<img.*?src="(.*?)".*?>"""
+    # res = r'src="https://\w+\.(jpg|gif|png|jpeg|bmp)"'
+    # res = r"(\s+?http://)?(\d*\.(jpg|gif|png|jpeg|bmp))"
     res_c = re.compile(res)
     jpg_url_list = re.findall(res_c,html)
     print (jpg_url_list)
-    for imgurl in jpg_url_list:
+    for imgurl in tqdm(jpg_url_list):
         urllib.request.urlretrieve(imgurl,"./1.jpg")
 
 # print (getHtml(url_dota))
